6bdacb410b03997f8aa6601f55a70f100516730f
lov-daniel <lovdaniel11@gmail.com>
2023-11-25 16:27:47 -0800

    laying down foundation

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template
 
 debug = False
 
-
-
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
 
---
+++ static/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    alert('Hey')
+  });
+
+function loseWeight() {
+    console.log("Losing weight");
+}
+
+function maintainWeight() {
+    console.log("Maintain weight");
+}
+
+function gainWeight() {
+    console.log("Gainig weight")
+}
