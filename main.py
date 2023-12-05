bd9d867c468f83ee29d6aadb86a34aee52298382
lov-daniel <lovdaniel11@gmail.com>
2023-12-04 23:11:18 -0800

    implementing javascript

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-debug = False
+debug = True
 
 app = Flask(__name__)
 
--- static/index.js
+++ static/index.js
@@ -1,15 +1,71 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    alert('Hey')
-  });
 
-function loseWeight() {
-    console.log("Losing weight");
+let weight = -1;
+let allergic = -1;
+
+const lifestyles = ["Vegan", "Vegetarian", "Neither"];
+
+const allergies = ["Seafood", "Nut", "Fruit", "Wheat",
+                   "Gluten", "Soy", "Sesame", "Shellfish"];
+
+function weightChoice(userChoice) {
+    console.log(userChoice);
+    switch(userChoice) {
+
+        case 0:
+            weight = 0;
+            break;
+        case 1:
+            weight = 1;
+            break;
+        case 2:
+            weight = 2;
+            break;
+    }
+
+    let buttongroup2 = document.getElementById("buttongroup2");
+
+    for (let index = 0; index <lifestyles.length; index++) {
+        let parameter = index;
+        let button = document.createElement("button");
+        button.addEventListener("click", function(e) {
+
+            switch (parameter) {
+
+                case 0:
+                    allergicTable("vegan");
+            }
+
+            allergicTable();
+        });
+        button.textContent = `${lifestyles[index]}`
+        buttongroup2.append(button);
+    }
+
+    buttongroup2.style.visibility = "visible";
+
+    console.log(weight);
 }
 
-function maintainWeight() {
-    console.log("Maintain weight");
+function boo(index) {
+
+    alert(index);
 }
 
-function gainWeight() {
-    console.log("Gainig weight")
+function allergicTable(userChoice) {
+
+    switch(userChoice) {
+
+        case 0:
+            allergic = 0;
+            break;
+        case 1:
+            allergic = 1;
+            break;
+        }
+    if (allergic === 1) {
+
+        let checktable = document.getElementById("allergies");
+
+        
+    }
 }
